fb88ce324fcde26cc3b5c5a18a632bf85863739a
astorozhevsky <astorozhevsky@ronasit.com>
2020-01-23 13:14:38 +0600

    minor fix for components

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,8 +1,22 @@
 <template>
-    <Header>
-    <Content/> 
-    <Footer>
+  <div>
+    <Header />
+    <Content />
+    <Footer />
+  </div>
 </template> 
+
+<script>
+import Header from '@theme/components/Header.vue'
+import Footer from '@theme/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
 
 <style lang="scss">
 h1 {
