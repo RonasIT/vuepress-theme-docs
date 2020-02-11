0ac7e14bdd32efddf5750384444c0c2fcfa02b9e
astorozhevsky <astorozhevsky@ronasit.com>
2020-02-11 14:23:17 +0600

    added DocId global-component

---
+++ global-components/DocId.vue
@@ -0,0 +1,25 @@
+<template>
+  <Badge :text="docId" />
+</template> 
+
+<script>
+import Badge from "@parent-theme/global-components/Badge.vue";
+
+export default {
+  components: {
+    Badge
+  },
+  data: function() {
+    return {
+      docId: ""
+    };
+  },
+  mounted() {
+    const relativePathParts = this.$page.relativePath.split("/");
+    const filePath = relativePathParts[relativePathParts.length - 1];
+    const filePathParts = filePath.split(".");
+
+    this.docId = filePathParts[0].toUpperCase();
+  }
+};
+</script>
